b69a7aa195fdb56b1224e604c74fb41c5ef6db0e
vikrant <vikrant.verma@pwc.com>
2023-07-20 19:37:37 +0530

    file added

---
+++ aws/components/s3/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  cloud {
+    organization = "gitlab-poc"
+
+    workspaces {
+      name = "testing-aws-oidc"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-xyxyxyxyjhjsdghgs"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
---
+++ aws/components/s3/variable.tf
@@ -0,0 +1,16 @@
+
+variable "TFC_AWS_PROVIDER_AUTH" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+variable "TFC_AWS_RUN_ROLE_ARN" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+
