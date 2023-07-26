bbae8d6e1f1cead3cf2e23ba835f339e0af30c41
vikrant <vikrant.verma@pwc.com>
2023-07-26 11:24:40 +0530

    test

---
+++ aws/components/lambda-layer/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "gitlab-poc"
+
+    workspaces {
+      name = "lamnda-layer"
+    }
+  }
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "null_resource" "install_dependencies" {
+  provisioner "local-exec" {
+    command = "chmod +x test.sh; ./test.sh"
+    interpreter = ["bash", "-c"] 
+
+  }
+}
