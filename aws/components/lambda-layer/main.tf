b0c7226d4cdc765bc8648a94543bb2ddb8fae47d
vikrant <vikrant.verma@pwc.com>
2023-07-26 12:34:56 +0530

    s

--- aws/components/lambda-layer/main.tf
+++ aws/components/lambda-layer/main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   cloud {
     organization = "gitlab-poc"
@@ -22,10 +23,34 @@ provider "aws" {
 
 
 
-resource "null_resource" "install_dependencies" {
-  provisioner "local-exec" {
-    command = "yum install zip -y ; apt install zip -y; chmod +x test.sh; ./test.sh"
-    interpreter = ["bash", "-c"] 
 
+
+resource "null_resource" "lambda_exporter" {
+  provisioner "local-exec" {
+    command = "chmod +x ${path.module}/lambda-files/test.sh; ${path.module}/lambda-files/test.sh"
+    interpreter = ["bash", "-c"] 
   }
+  triggers = {
+    index = "${base64sha256(file("${path.module}/lambda-files/test.sh"))}"
+  }
+}
+
+
+
+data "null_data_source" "wait_for_lambda_exporter" {
+  inputs = {
+    # This ensures that this data resource will not be evaluated until
+    # after the null_resource has been created.
+    lambda_exporter_id = "${null_resource.lambda_exporter.id}"
+
+    # This value gives us something to implicitly depend on
+    # in the archive_file below.
+    source_dir = "${path.module}/panda-layer/"
+  }
+}
+
+data "archive_file" "lambda_exporter" {
+  output_path = "${path.module}/lambda-files.zip"
+  source_dir  = "${data.null_data_source.wait_for_lambda_exporter.outputs["source_dir"]}"
+  type        = "zip"
 }
